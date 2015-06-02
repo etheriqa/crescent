5816c4f5887871cb911bffb45af046406a3b1679
Kaede Takamori <etheriqa@gmail.com>
2015-06-03 01:28:57 +0900

    Add stats getter

--- unit.go
+++ unit.go
@@ -13,8 +13,8 @@ type unit struct {
 	unitName       string
 	group          uint8
 	seat           uint8
-	baseStats      unitStatistics
-	currentStats   unitStatistics
+	stats          unitStatistics
+	mod            unitModification
 	operators      map[operator]interface{}
 	statsSubject   subject
 	disableSubject subject
@@ -31,6 +31,55 @@ type unitStatistics struct {
 	criticalStrikeDamage int32
 	cooldownReduction    int32
 	threatFactor         int32
+}
+
+type unitModification struct {
+	armor                int32
+	magicResistance      int32
+	criticalStrikeChance int32
+	criticalStrikeDamage int32
+	cooldownReduction    int32
+	threatFactor         int32
+}
+
+func (u *unit) health() int32 {
+	return u.stats.health
+}
+
+func (u *unit) healthRegeneration() int32 {
+	return u.stats.healthRegeneration
+}
+
+func (u *unit) mana() int32 {
+	return u.stats.mana
+}
+
+func (u *unit) manaRegeneration() int32 {
+	return u.stats.mana
+}
+
+func (u *unit) armor() int32 {
+	return u.stats.armor + u.mod.armor
+}
+
+func (u *unit) magicResistance() int32 {
+	return u.stats.magicResistance + u.mod.magicResistance
+}
+
+func (u *unit) criticalStrikeChance() int32 {
+	return u.stats.criticalStrikeChance + u.mod.criticalStrikeChance
+}
+
+func (u *unit) criticalStrikeDamage() int32 {
+	return u.stats.criticalStrikeDamage + u.mod.criticalStrikeDamage
+}
+
+func (u *unit) cooldownReduction() int32 {
+	return u.stats.cooldownReduction + u.mod.cooldownReduction
+}
+
+func (u *unit) threatFactor() int32 {
+	return u.stats.threatFactor + u.mod.threatFactor
 }
 
 func (u *unit) attachOperator(o operator) {
