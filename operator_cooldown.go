7666fb98728f5d1b928d40e854eb58a63e687514
Kaede Takamori <etheriqa@gmail.com>
2015-06-07 19:24:24 +0900

    Do not detach cooldown operators on dying

--- operator_cooldown.go
+++ operator_cooldown.go
@@ -6,7 +6,6 @@ type cooldown struct {
 }
 
 func (c *cooldown) onAttach() {
-	c.addEventHandler(c, eventDead)
 	c.addEventHandler(c, eventGameTick)
 	c.publish(message{
 		// TODO pack message
@@ -16,15 +15,12 @@ func (c *cooldown) onAttach() {
 
 // onDetach removes the eventHandler
 func (c *cooldown) onDetach() {
-	c.removeEventHandler(c, eventDead)
 	c.removeEventHandler(c, eventGameTick)
 }
 
 // handleEvent handles the event
 func (c *cooldown) handleEvent(e event) {
 	switch e {
-	case eventDead:
-		c.detachOperator(c)
 	case eventGameTick:
 		c.expire(c, message{
 			// TODO pack message
