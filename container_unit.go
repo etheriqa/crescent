5a31ee7a5fdca65db4904717e0c4db15fa17ad2f
Kaede Takamori <etheriqa@gmail.com>
2015-06-14 18:12:09 +0900

    Add UnitMap that implements UnitContainer

--- container_unit.go
+++ container_unit.go
@@ -5,3 +5,35 @@ type UnitContainer interface {
 	EachFriend(*Unit, func(*Unit))
 	EachEnemy(*Unit, func(*Unit))
 }
+
+type UnitMap map[UnitID]*Unit
+
+// MakeUnitMap returns a UnitMap
+func MakeUnitMap() UnitMap {
+	return make(map[UnitID]*Unit)
+}
+
+// Each calls the callback function with each the Unit
+func (um UnitMap) Each(callback func(*Unit)) {
+	for _, u := range um {
+		callback(u)
+	}
+}
+
+// EachFriend calls the callback function with each the friend Unit
+func (um UnitMap) EachFriend(u *Unit, callback func(*Unit)) {
+	for _, v := range um {
+		if u.Group() == v.Group() {
+			callback(u)
+		}
+	}
+}
+
+// EachEnemy calls the callback function with each the enemy Unit
+func (um UnitMap) EachEnemy(u *Unit, callback func(*Unit)) {
+	for _, v := range um {
+		if u.Group() != v.Group() {
+			callback(u)
+		}
+	}
+}
